550474a1bc38cbe2b2c506d698c7f11c1c3fa1c8
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-12-20 23:31:38 +0900

    feat : Add HelloController
    Create HelloController.java and MvcTest

---
+++ src/test/java/likelion/sns/controller/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package likelion.sns.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Hello Controller GET Mapping 테스트")
+    void helloTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/hello"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
+    }
+}
