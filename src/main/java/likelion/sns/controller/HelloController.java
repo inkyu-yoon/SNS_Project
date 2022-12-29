15cae603710cd1b57d6ae27c777bf084aa49a35a
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-12-29 09:45:40 +0900

    feat : Add sum of digit

--- src/main/java/likelion/sns/controller/HelloController.java
+++ src/main/java/likelion/sns/controller/HelloController.java
@@ -1,9 +1,12 @@
 package likelion.sns.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/v1/hello")
@@ -13,5 +16,13 @@ public class HelloController {
     @GetMapping("")
     public String hello() {
         return "윤인규";
+    }
+
+    @GetMapping("/{num}")
+    public String hello(@PathVariable(name = "num") String num) {
+
+        int sum = Arrays.stream(num.split("")).mapToInt(s -> Integer.valueOf(s)).sum();
+
+        return String.valueOf(sum);
     }
 }
